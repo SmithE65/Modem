a716cca1c80c94f180d24fb0e23d9f1c383d6807
Eric Smith <ydrive65@yahoo.com>
2019-02-23 10:00:17 -0500

    Add project files.

---
+++ Modem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
